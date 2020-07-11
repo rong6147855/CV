5b010d3bd8fed2fd15ebaf6aa346308a9e641da1
yangrong <rong6147855@foxmail.com>
2020-07-11 23:10:20 +0800

    update  build

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -181,7 +181,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61979" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54346" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -358,4 +358,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
